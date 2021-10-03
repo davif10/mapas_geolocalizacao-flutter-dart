d48cc3774eac8b5bb63908623132665ad50c759c
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-10-03 20:26:03 -0300

    -Adicionando marcadores

--- mapas_geolocalizacao/lib/Home.dart
+++ mapas_geolocalizacao/lib/Home.dart
@@ -10,7 +10,10 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //-23.701617847651825, -46.69866983421911
+  //-23.706150624867185, -46.688411752494154 estacao
+  //-23.706360904523923, -46.70196160567839 hospital
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _marcadores ={};
 
   _onMapCreated(GoogleMapController googleMapController){
     _controller.complete(googleMapController);
@@ -21,13 +24,58 @@ class _HomeState extends State<Home> {
     googleMapController.animateCamera(
       CameraUpdate.newCameraPosition(
           CameraPosition(
-              target: LatLng(-23.701617847651825, -46.69866983421911),
-              zoom: 19,
+              target: LatLng(-23.706150624867185, -46.688411752494154),
+              zoom: 15,
               tilt: 30,
               bearing: 30
           )
       )
     );
+  }
+
+  _carregarMarcadores(){
+    Set<Marker> marcadoresLocal ={};
+    Marker marcadorEstacao = Marker(
+      markerId: MarkerId("marcador-estacao"),
+      position: LatLng(-23.706150624867185, -46.688411752494154),
+      infoWindow: InfoWindow(
+        title: "Estação Autódromo"
+      ),
+      icon: BitmapDescriptor.defaultMarkerWithHue(
+          BitmapDescriptor.hueMagenta
+      ),
+      rotation: 45,
+      onTap: (){
+        print("Estação clicado");
+      }
+    );
+
+    Marker marcadorHospital = Marker(
+        markerId: MarkerId("marcador-hospital"),
+        position: LatLng(-23.706360904523923, -46.70196160567839),
+        infoWindow: InfoWindow(
+            title: "Hospital Maternidade Interlagos"
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueBlue
+        ),
+        onTap: (){
+          print("Hospital clicado");
+        }
+    );
+
+    marcadoresLocal.add(marcadorEstacao);
+    marcadoresLocal.add(marcadorHospital);
+
+    setState(() {
+      _marcadores = marcadoresLocal;
+    });
+  }
+
+  @override
+  void initState() {
+    _carregarMarcadores();
+    super.initState();
   }
 
   @override
@@ -40,12 +88,12 @@ class _HomeState extends State<Home> {
         mapType: MapType.normal,
         initialCameraPosition: CameraPosition(
           target: LatLng(
-              -23.701617847651825,
-              -46.69866983421911
+              -23.706150624867185, -46.688411752494154
           ),
-          zoom: 16
+          zoom: 15
         ),
         onMapCreated: _onMapCreated,
+        markers: _marcadores,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _movimentarCamera,
